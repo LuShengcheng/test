e69938183553a2c0c67f3f6419954ce6b82df633
lushengcheng <lushengcheng@hotmail.com>
2022-07-12 23:23:14 +0800

    添加DockerFile

--- src/main/java/cn/lushengcheng/test/TestApplication.java
+++ src/main/java/cn/lushengcheng/test/TestApplication.java
@@ -3,11 +3,14 @@ package cn.lushengcheng.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author lushengcheng
+ */
 @SpringBootApplication
 public class TestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
 
 }
--- src/main/java/cn/lushengcheng/test/controller/TestController.java
+++ src/main/java/cn/lushengcheng/test/controller/TestController.java
@@ -1,8 +1,31 @@
-package cn.lushengcheng.test.controller;/**
- *<p>
- *     测试
- *</p>
+package cn.lushengcheng.test.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 测试
+ * </p>
+ *
  * @author lushengcheng
- * @since  2022/7/12 20:17
- **/public class TestController {
+ * @since 2022/7/12 20:17
+ **/
+@RestController
+@Slf4j
+@RequestMapping("/test")
+public class TestController {
+
+    /**测试控制器
+     * Get请求
+     * 返回test字符串
+     */
+    @GetMapping("/test")
+    public String test() {
+        log.info("test");
+
+        return "test";
+    }
 }
